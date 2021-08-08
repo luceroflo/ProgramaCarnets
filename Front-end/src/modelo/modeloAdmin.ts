51b2c3934b90e60bbacecda7be58113bc390dd27
Detro98 <ragajeremy@gmail.com>
2021-08-08 11:16:34 -0400

    Modelo admin

---
+++ Front-end/src/modelo/modeloAdmin.ts
@@ -0,0 +1,11 @@
+export interface adminModel {
+    username: String,
+    nombre: String,
+    apellido: String,
+    cedula: Number,
+    correo: String,
+    telf_1: String,
+    telf_2: String,
+    password: String,
+    co_password: String
+}
