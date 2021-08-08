aa2c2d4967a8e02c50a26658f0da849369088f8c
Detro98 <ragajeremy@gmail.com>
2021-08-08 11:16:18 -0400

    Get users copy paste

---
+++ Front-end/src/funciones/getUsers.ts
@@ -0,0 +1,24 @@
+import { ref } from "vue"
+
+const getUsers = () => {
+    const users = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+    try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('No data available')
+        }
+        users.value = await data.json()
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    return { users, error, load }
+}
+
+export default getUsers
